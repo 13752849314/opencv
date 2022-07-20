10fc8a844462804dbf984bf341ad852e86c1e682
13752849314 <371575373@qq.com>
2022-07-20 18:21:08 +0800

    2022/07/20

---
+++ cmake-build-debug/CMakeFiles/opencv.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opencv.dir/code/c++/main.cpp.obj"
+  "libopencv.dll.a"
+  "opencv.exe"
+  "opencv.exe.manifest"
+  "opencv.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opencv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
