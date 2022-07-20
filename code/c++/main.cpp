d1afe5a35aae925c65e7ea66c7083604812362ff
13752849314 <371575373@qq.com>
2022-07-20 23:48:34 +0800

    2022/07/20

--- code/c++/main.cpp
+++ code/c++/main.cpp
@@ -3,6 +3,10 @@
 //
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+
 
 using namespace cv;
 using namespace std;
@@ -10,7 +14,7 @@ using namespace std;
 int main() {
     cout << 123 << endl;
     Mat image = imread(R"(D:\github\opencv\image\test.jpg)");
-    namedWindow("hg", WINDOW_NORMAL);
+    namedWindow("hg", WINDOW_AUTOSIZE);
     imshow("hg", image);
 
     waitKey(0);
